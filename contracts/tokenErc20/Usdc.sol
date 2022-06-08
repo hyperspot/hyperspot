160df370bb71f5423fac51d84195e35313a3fe79
hyperspot <100395044+hyperspot@users.noreply.github.com>
2022-06-08 09:04:38 +0800

    Update Usdc.sol

--- contracts/tokenErc20/Usdc.sol
+++ contracts/tokenErc20/Usdc.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 contract Usdc is ERC20{
     address public admin;
     event AdminChange(address indexed Admin, address indexed newAdmin);

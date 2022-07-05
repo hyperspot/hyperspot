02b9b45c68fde71815c1c35d6f0f3d5be6f8e266
hyperspot <100395044+hyperspot@users.noreply.github.com>
2022-07-05 09:16:22 +0800

    Update Eth.sol

--- contracts/tokenErc20/Eth.sol
+++ contracts/tokenErc20/Eth.sol
@@ -10,14 +10,17 @@ contract Eth is ERC20{
         admin = manager;
         _mint(manager, 100_000_000 *10 ** 18);
     }
+    
     modifier  _isOwner() {
         require(msg.sender == admin);
         _;
     }
+    
     function changeOwner(address manager) external _isOwner {
         admin = manager;
         emit AdminChange(msg.sender,manager);
     }
+    
     function burn(address account, uint256 amount) external _isOwner{
         _burn(account, amount);
     }

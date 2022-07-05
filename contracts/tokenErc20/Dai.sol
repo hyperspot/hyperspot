f063c48a61734498b9fe92a369a283f9e7ba7a3d
hyperspot <100395044+hyperspot@users.noreply.github.com>
2022-07-05 09:15:54 +0800

    Update Dai.sol

--- contracts/tokenErc20/Dai.sol
+++ contracts/tokenErc20/Dai.sol
@@ -14,10 +14,12 @@ contract Dai is ERC20{
         require(msg.sender == admin);
         _;
     }
+    
     function changeOwner(address manager) external _isOwner {
         admin = manager;
         emit AdminChange(msg.sender,manager);
     }
+    
     function burn(address account, uint256 amount) external _isOwner{
         _burn(account, amount);
     }

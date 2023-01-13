3d8489ebd06d0f0d42a79cd53db151358dc18b77
hyperspot <100395044+hyperspot@users.noreply.github.com>
2023-01-13 17:06:17 +0800

    Update App.vue

--- Hyperspot-Front-end/src/App.vue
+++ Hyperspot-Front-end/src/App.vue
@@ -1,1 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
 
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    color: #fff;
+  }
+</style>
